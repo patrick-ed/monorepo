bd437f0882bb094be533415a60fb33ae29efc3bb
patrick-ed <patrickedumdum@gmail.com>
2025-05-05 21:16:21 +0100

    build on pi and not on actions

--- webapps/fullstackproject/deploy.sh
+++ webapps/fullstackproject/deploy.sh
@@ -9,54 +9,29 @@ APP_NAME="fullstackproject"
 LOCAL_DIR="webapps/$APP_NAME"
 REMOTE_DIR="/home/$USER/monorepo/webapps/$APP_NAME"
 
-# Build images
-echo "Building Docker images..."
-docker buildx build \
-  --platform linux/arm64 \
-  --cache-from=type=local,src=.buildx-cache \
-  --cache-to=type=local,dest=.buildx-cache \
-  --output type=docker \
-  -t fsp-frontend:latest \
-  "$LOCAL_DIR/frontend"
-  
-docker buildx build \
-  --platform linux/arm64 \
-  --cache-from=type=local,src=.buildx-cache \
-  --cache-to=type=local,dest=.buildx-cache \
-  --output type=docker \
-  -t fsp-backend:latest \
-  "$LOCAL_DIR/backend"
-
-# Save images to tarballs
-echo "Saving images to tarballs..."
-docker save fsp-frontend:latest | gzip > fsp-frontend.tar.gz
-docker save fsp-backend:latest | gzip > fsp-backend.tar.gz
-
-# Upload images and project files to Raspberry Pi
-echo "Uploading images and project files to Raspberry Pi..."
-scp -P "$PORT" fsp-frontend.tar.gz fsp-backend.tar.gz "pi-deploy:/home/$USER/temp/"
+# Upload project files to Raspberry Pi
+echo "Uploading project files to Raspberry Pi..."
 scp -P "$PORT" -r "$LOCAL_DIR" "pi-deploy:/home/$USER/monorepo/webapps/"
-
-rm fsp-frontend.tar.gz fsp-backend.tar.gz
 
 # Running deployment on Raspberry Pi
 echo "Running deployment on Raspberry Pi..."
 ssh -p "$PORT" "pi-deploy" <<EOF
   set -e
 
-  echo "Loading Docker images..."
-  gunzip -c /home/$USER/temp/fsp-frontend.tar.gz | docker load
-  gunzip -c /home/$USER/temp/fsp-backend.tar.gz | docker load
-  rm /home/$USER/temp/fsp-frontend.tar.gz /home/$USER/temp/fsp-backend.tar.gz
-
   echo "Navigating to project directory..."
   cd "$REMOTE_DIR"
+
+  echo "Building Docker images..."
+  docker compose build
 
   echo "Pulling down containers..."
   docker compose down || true
 
   echo "Starting containers..."
-  docker compose up -d
+  docker compose up -d --build
+
+  echo "Cleaning up unused images..."
+  docker image prune -f
 
   echo "Deployment complete"
 EOF
--- webapps/main/deploy.sh
+++ webapps/main/deploy.sh
@@ -9,45 +9,29 @@ APP_NAME="main"
 LOCAL_DIR="webapps/$APP_NAME"
 REMOTE_DIR="/home/$USER/monorepo/webapps/$APP_NAME"
 
-# Build images
-echo "Building Docker images..."
-docker buildx build \
-  --platform linux/arm64 \
-  --cache-from=type=local,src=.buildx-cache \
-  --cache-to=type=local,dest=.buildx-cache \
-  --output type=docker \
-  -t main-frontend:latest \
-  "$LOCAL_DIR/frontend"
-
-
-# Save images to tarballs
-echo "Saving images to tarballs..."
-docker save main-frontend:latest | gzip > main-frontend.tar.gz
-
-# Upload images and project files to Raspberry Pi
-echo "Uploading images and project files to Raspberry Pi..."
-scp -P "$PORT" main-frontend.tar.gz "pi-deploy:/home/$USER/temp/"
+# Upload project files to Raspberry Pi
+echo "Uploading project files to Raspberry Pi..."
 scp -P "$PORT" -r "$LOCAL_DIR" "pi-deploy:/home/$USER/monorepo/webapps/"
-
-rm main-frontend.tar.gz
 
 # Running deployment on Raspberry Pi
 echo "Running deployment on Raspberry Pi..."
 ssh -p "$PORT" pi-deploy <<EOF
   set -e
 
-  echo "Loading Docker images..."
-  gunzip -c /home/$USER/temp/main-frontend.tar.gz | docker load
-  rm /home/$USER/temp/main-frontend.tar.gz
-
   echo "Navigating to project directory..."
   cd "$REMOTE_DIR"
+
+  echo "Building Docker images..."
+  docker compose build
 
   echo "Pulling down containers..."
   docker compose down || true
 
   echo "Starting containers..."
-  docker compose up -d
+  docker compose up -d --build
+
+  echo "Cleaning up unused images..."
+  docker image prune -f
 
   echo "Deployment complete"
 EOF
