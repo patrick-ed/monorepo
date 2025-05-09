73c53248524dcb98c917189594e803289218cd17
patrick-ed <patrickedumdum@gmail.com>
2025-05-09 23:09:19 +0100

    added non-sensitive env files

--- webapps/fullstackproject/frontend/src/app/modules/auth/auth.component.ts
+++ webapps/fullstackproject/frontend/src/app/modules/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-auth',
@@ -9,8 +10,9 @@ import { Component } from '@angular/core';
 export class AuthComponent {
 
   authenticated = false
+  loginUri = environment.apiUrl + '/login'
 
   ngOnInit() {
     localStorage.getItem('token') ? this.authenticated = true : this.authenticated = false
-  }  
+  }
 }
