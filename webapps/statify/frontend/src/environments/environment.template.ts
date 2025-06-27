d2fc1ba4554332d3e866da4730979183a6e2feed
patrick-ed <patrickedumdum@gmail.com>
2025-06-27 22:43:50 +0100

    added envs

---
+++ webapps/statify/frontend/src/environments/environment.template.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    PRODUCTION: null,
+    SPOTIFY: {
+        REDIRECT_URI: 'REPLACE',
+        BASE_URL: 'REPLACE',
+        AUTH_URL: 'REPLACE',
+        CLIENT_ID: 'REPLACE',
+        CLIENT_SECRET: 'REPLACE',
+    },
+};
