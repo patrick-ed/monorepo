47f22a96b036fbcbf45e9f0e0192a15e6ed5ea25
patrick-ed <patrickedumdum@gmail.com>
2025-06-27 01:58:18 +0100

    add ForceGraphData

--- webapps/statify/frontend/src/app/core/d3/interfaces.ts
+++ webapps/statify/frontend/src/app/core/d3/interfaces.ts
@@ -1,5 +1,13 @@
+import { HierarchyLink } from "d3";
+
 export interface ForceNodeDatum {
     name: string;
     children?: ForceNodeDatum[];
     value?: number;
+}
+
+
+export interface ForceGraphData {
+    nodes: ForceNodeDatum[];
+    links: HierarchyLink<ForceNodeDatum>[];
 }
