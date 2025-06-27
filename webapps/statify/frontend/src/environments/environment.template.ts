b8331f6e88c66f28d201bbadaac5f2b3e7fa2218
patrick-ed <patrickedumdum@gmail.com>
2025-06-28 00:23:27 +0100

    upd to use 127.0.0.1 in favour of localhost

--- webapps/statify/frontend/src/environments/environment.template.ts
+++ webapps/statify/frontend/src/environments/environment.template.ts
@@ -1,9 +1,10 @@
 export const ENV = {
-    PRODUCTION: null,
+    PRODUCTION: false,
     SPOTIFY: {
-        REDIRECT_URI: 'REPLACE',
-        BASE_URL: 'REPLACE',
-        AUTH_URL: 'REPLACE',
+        REDIRECT_URI: 'http://statify.patrickdd.com/callback OR http://127.0.0.1:4200/callback',
+        BASE_URL: 'https://api.spotify.com/v1',
+        AUTH_URL: 'https://accounts.spotify.com/authorize',
+        TOKEN_URL: 'https://accounts.spotify.com/api/token',
         CLIENT_ID: 'REPLACE',
         CLIENT_SECRET: 'REPLACE',
     },
