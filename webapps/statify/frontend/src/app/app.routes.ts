24090c001b7f57f60f06abfc7096dfee778ca26c
patrick-ed <patrickedumdum@gmail.com>
2025-06-29 21:32:40 +0100

    [Statify][Feature] Added dashboard route

--- webapps/statify/frontend/src/app/app.routes.ts
+++ webapps/statify/frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './features/home/components/home.component';
 import { GraphComponent } from './features/graph/graph.component';
 import { Graph3dComponent } from './features/graph3d/graph3d.component';
 import { CallbackComponent } from './features/spotify/callback/callback.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'graph', component: GraphComponent },
     { path: 'graph3d', component: Graph3dComponent },
-    { path: 'callback', component: CallbackComponent }
+    { path: 'callback', component: CallbackComponent },
+    { path: 'dashboard', component: DashboardComponent }
 ];
