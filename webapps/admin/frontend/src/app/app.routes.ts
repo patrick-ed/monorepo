d4f9d7051d02cb3e4b89473b38dc5e3f9057ce7e
patrick-ed <patrickedumdum@gmail.com>
2025-08-24 22:45:28 +0100

    [Admin][Feat] Remove some skeleton UI.

--- webapps/admin/frontend/src/app/app.routes.ts
+++ webapps/admin/frontend/src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/components/home.component';
-import { PingComponent } from './features/ping/components/ping.component';
-import { PingPageComponent } from './features/ping/pages/ping-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'ping', component: PingPageComponent },
 
     { path: '**', redirectTo: '' },
 ];
