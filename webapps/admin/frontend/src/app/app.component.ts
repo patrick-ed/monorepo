e3372a9dcabcfb1663f594af565ac175b6886807
patrick-ed <patrickedumdum@gmail.com>
2025-08-25 22:24:19 +0100

    [Admin][Feat] Added basic header + mock login status.

--- webapps/admin/frontend/src/app/app.component.ts
+++ webapps/admin/frontend/src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html'
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'template';
+  loggedin = false;
+
+  toggleLogin() {
+    this.loggedin = !this.loggedin;
+  }
 }
