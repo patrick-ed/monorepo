6da3d1bfa7c9ff995c68542541cf5f490a1cf575
patrick-ed <patrickedumdum@gmail.com>
2026-01-31 20:18:06 +0000

    [portfolio][Feature] Dark / Light mode theming

--- webapps/portfolio/frontend/src/app/app.component.ts
+++ webapps/portfolio/frontend/src/app/app.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser, NgIf } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Theme } from './core/enums/theme.enum';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet/>',
+  standalone: true,
+  imports: [RouterOutlet, NgIf],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  theme: Theme = Theme.Dark;
+  Theme = Theme;
+  THEME_KEY = "theme"
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      const storedTheme = localStorage.getItem(this.THEME_KEY);
+      if (storedTheme) {
+        this.theme = storedTheme as Theme;
+      }
+      this.updateThemeAttribute();
+    }
+  }
+
+  toggleTheme() {
+    this.theme = this.theme === Theme.Dark ? Theme.Light : Theme.Dark;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(this.THEME_KEY, this.theme);
+    }
+    this.updateThemeAttribute();
+  }
+
+  private updateThemeAttribute() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.theme === Theme.Dark) {
+        this.renderer.setAttribute(document.documentElement, 'data-theme', 'dark');
+      } else {
+        this.renderer.removeAttribute(document.documentElement, 'data-theme');
+      }
+    }
+  }
 }
