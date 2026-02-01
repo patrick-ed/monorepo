64b0e54350106b92b507fc2fecea784558cfdbfb
patrick-ed <patrickedumdum@gmail.com>
2026-02-01 02:15:34 +0000

    [portfolio][Feature] Styling changes.

--- webapps/portfolio/frontend/src/app/app.routes.ts
+++ webapps/portfolio/frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
 import { AboutComponent } from './pages/about/about.component';
+import { LandingComponent } from './pages/landing/landing.component';
 import { LegacyComponent } from './pages/legacy/legacy.component';
 
 export const routes: Routes = [
