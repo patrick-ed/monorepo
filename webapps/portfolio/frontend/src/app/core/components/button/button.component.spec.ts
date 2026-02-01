7c55b8631b732e6cbb47e9b26e4b1082abeb33d7
patrick-ed <patrickedumdum@gmail.com>
2026-02-01 16:18:54 +0000

    [portfolio][Feature] Switched to self-hosting font + about page changes.

--- webapps/portfolio/frontend/src/app/core/components/button/button.component.spec.ts
+++ webapps/portfolio/frontend/src/app/core/components/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {CardComponent} from '../card/card.component';
 
-import { CardComponent } from './card.component';
+
 
 describe('CardComponent', () => {
   let component: CardComponent;
--- webapps/portfolio/frontend/src/app/pages/about/about.component.ts
+++ webapps/portfolio/frontend/src/app/pages/about/about.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import {CardComponent} from '../../core/components/card/card.component';
+import {CommonModule} from '@angular/common';
+
+interface SocialLink {
+  name: string,
+  link: string,
+}
 
 @Component({
   selector: 'app-about',
   imports: [
-    CardComponent
+    CardComponent,
+    CommonModule
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  linkedin: SocialLink = {name: 'Linkedin', link: 'https://www.linkedin.com/in/patricked/'};
+  github: SocialLink = {name: 'Github', link: 'https://github.com/patrick-ed/'};
+  leetcode: SocialLink = {name: 'Leetcode', link: 'https://leetcode.com/u/patrick-ed/'};
+  email: SocialLink = {name: 'Email', link: 'mailto:patrickedumdum@gmail.com'};
+
+  socialLinks: SocialLink[] = [this.linkedin, this.github, this.leetcode, this.email];
 
 }
